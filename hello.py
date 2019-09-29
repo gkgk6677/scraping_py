23def10ffe6ae1f086b96e851e0743e4eb248c5e
seo <gkgk6677@naver.com>
2019-09-30 01:28:38 +0900

    select 박스에 있는 내용을 지정한 scraping code

--- hello.py
+++ hello.py
@@ -5,3 +5,7 @@ from selenium.webdriver.support.ui import Select
 driver = webdriver.Chrome('./chromedriver')
 driver.implicitly_wait(3)
 driver.get('https://naver.com')
+
+# F12 관리자 모드에 들어가서 로그인 버튼을 선택 후 xpath로 복사하여 경로값 넣어준다.
+element = Select(driver.find_element_by_xpath("//*[@id='account']/div/a"))
+driver.execute_script("arguments[0].click();")
